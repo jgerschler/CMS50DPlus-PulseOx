76efe6ba2656a58ac5652f0e9f6dafda4138997e
jgerschler <jjgerschler@gmail.com>
2017-01-08 11:39:45 -0600

    exploring plotting options

--- Plotter.py
+++ Plotter.py
@@ -1,21 +1,40 @@
+##import matplotlib.pyplot as plt
+##import matplotlib.animation as animation
+##import time
+##
+##fig = plt.figure()
+##ax1 = fig.add_subplot(1,1,1)
+##
+##def animate(i):
+##    xar = [1,2,3,4,5]
+##    yar = [2,4,8,16,32]
+##    ax1.clear()
+##    ax1.plot(xar,yar)
+##ani = animation.FuncAnimation(fig, animate, interval=1000)
+##plt.show()
+##
+##time.sleep(3)
+
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import time
 
-fig = plt.figure()
-ax1 = fig.add_subplot(1,1,1)
+fig, ax = plt.subplots()
+
+x = np.arange(0, 2*np.pi, 0.01)
+line, = ax.plot(x, np.sin(x))
+
 
 def animate(i):
-    pullData = open("sampleText.txt","r").read()
-    dataArray = pullData.split('\n')
-    xar = []
-    yar = []
-    for eachLine in dataArray:
-        if len(eachLine)>1:
-            x,y = eachLine.split(',')
-            xar.append(int(x))
-            yar.append(int(y))
-    ax1.clear()
-    ax1.plot(xar,yar)
-ani = animation.FuncAnimation(fig, animate, interval=1000)
+    line.set_ydata(np.sin(x + i/10.0))  # update the data
+    return line,
+
+
+# Init only required for blitting to give a clean slate.
+def init():
+    line.set_ydata(np.ma.array(x, mask=True))
+    return line,
+
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
+                              interval=25, blit=True)
 plt.show()
