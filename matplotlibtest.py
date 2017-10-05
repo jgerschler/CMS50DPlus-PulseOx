e5a62477b6703938fc4384d98e9cf5d61063b68a
Jared Gerschler <jjgerschler@gmail.com>
2017-10-05 11:21:19 -0500

    testing new plotting setup

---
+++ matplotlibtest.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def data_gen(t=0):
+    cnt = 0
+    while cnt < 1000:
+        cnt += 1
+        t += 0.1
+        yield t, 100 * np.sin(2*np.pi*t) * np.exp(-t/10.)
+
+
+def init():
+    ax.set_ylim(40, 140)# change range!
+    ax.set_xlim(0, 10)
+    del xdata[:]
+    del ydata[:]
+    line.set_data(xdata, ydata)
+    return line,
+
+fig, ax = plt.subplots()
+line, = ax.plot([], [], lw=1)
+
+ax.set_title("Pulse and SpO2 Tracker")
+#ax.set_autoscaley_on(False)
+text_pulse = "Pulse:"
+text_spo2 = "SpO2:"
+props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+ax.text(0.05, 0.95, text_pulse, transform=ax.transAxes, fontsize=14,
+              color='blue', verticalalignment='top', bbox=props)
+ax.text(0.05, 0.85, text_spo2, transform=ax.transAxes, fontsize=14,
+              color='green', verticalalignment='top', bbox=props)
+
+#ax.grid() # add grid
+xdata, ydata = [], []
+
+
+def run(data):
+    # update the data
+    t, y = data
+    xdata.append(t)
+    ydata.append(y)
+    xmin, xmax = ax.get_xlim()
+
+    if t >= xmax:
+        ax.set_xlim(xmin, 2*xmax)
+        ax.figure.canvas.draw()
+    line.set_data(xdata, ydata)
+
+    return line,
+
+ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
+                              repeat=False, init_func=init)
+plt.show()
