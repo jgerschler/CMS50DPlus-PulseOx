7efb46f1093d7b8d2655662c779e0a8865d7ad80
Jared Gerschler <jjgerschler@gmail.com>
2017-10-05 13:36:43 -0500

    testing new plotting setup -- not functional!

--- draft.py
+++ draft.py
@@ -2,13 +2,14 @@
 
 #!/usr/bin/env python
 import argparse
-import datetime
+#import datetime
 import threading
 import time
 import random
 import serial
 import sys
-from dateutil import parser as dateparser
+#from dateutil import parser as dateparser
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -49,11 +50,24 @@ class CMS50Dplus(object):
         else:
             return ord(char)
 
-    def datapoint(self, time, data): 
+    def update_data(self, data):
+        t, y = data
+        xdata.append(t)
+        ydata.append(y)
+        xmin, xmax = ax.get_xlim()
+
+        if t >= xmax:
+            ax.set_xlim(xmin, 2*xmax)
+            ax.figure.canvas.draw()
+        line.set_data(xdata, ydata)
+
+        return line,
+
+    def get_datapoint(self, data): 
         if [d & 0x80 != 0 for d in data] != [True, False, False, False, False]:
            raise ValueError("Invalid data packet.")
 
-         # 1st byte
+        # 1st byte
         self.signal_strength = data[0] & 0x0f
         self.finger_out = bool(data[0] & 0x10)
         self.dropping_spo2 = bool(data[0] & 0x20)
@@ -76,8 +90,7 @@ class CMS50Dplus(object):
 
         return str(self.pulse_rate)+","+str(self.blood_spo2)
 
-    def run(self):
-        print("entering run")
+    def data_gen(self, t=0):# data gen function
         counter = 0
         try:
             self.connect()
@@ -92,8 +105,8 @@ class CMS50Dplus(object):
 
                 if byte & 0x80:
                     print("byte is good")
-                    if idx == 5 and packet[0] & 0x80:
-                        data = str(self.datapoint(datetime.datetime.utcnow(), packet)).split(',')
+                    if idx == 5 and packet[0] & 0x80:# fix here (set data)
+                        data = str(self.get_datapoint(packet)).split(',')
                         if len(self.x_array) > 5000:
                             self.x_array.pop(0)
                             self.pulse_array.pop(0)
@@ -104,10 +117,9 @@ class CMS50Dplus(object):
                         else:
                             self.x_array.append(counter)
                             self.pulse_array.append(int(data[0]))
-                            self.spo2_array.append(int(data[1]))
-
-                        time.sleep(1)                        
+                            self.spo2_array.append(int(data[1]))                      
                         counter += 1
+                        t += 1
                     packet = [0]*5
                     idx = 0
             
@@ -115,11 +127,31 @@ class CMS50Dplus(object):
                     packet[idx] = byte
                     idx += 1
             print("got to the end of byte")
-        except:
+        except:# update
             time.sleep(0.1)# faulty cable causes occasional disconnection/connection
             print("hitting except")
             pass
 
+    def run(self):
+        fig, ax = plt.subplots()
+        line, = ax.plot([], [], lw=1)
+
+        ax.set_title("Pulse and SpO2 Tracker")
+        #ax.set_autoscaley_on(False)
+        text_pulse = "Pulse:"
+        text_spo2 = "SpO2:"
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        ax.text(0.05, 0.95, text_pulse, transform=ax.transAxes, fontsize=14,
+                      color='blue', verticalalignment='top', bbox=props)
+        ax.text(0.05, 0.85, text_spo2, transform=ax.transAxes, fontsize=14,
+                      color='green', verticalalignment='top', bbox=props)
+
+        #ax.grid() # add grid
+        xdata, ydata = [], []
+
+        ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
+                              repeat=False, init_func=init)
+        plt.show()
 
 
 x_array = []# could use numpy arrays here
@@ -128,3 +160,5 @@ spo2_array = []
 
 new_instance = CMS50Dplus("COM3", x_array, pulse_array, spo2_array)# adjust COM port as needed
 new_instance.run()
+
+
--- matplotlibtest.py
+++ matplotlibtest.py
@@ -8,19 +8,22 @@ def data_gen(t=0):
     while cnt < 1000:
         cnt += 1
         t += 0.1
-        yield t, 100 * np.sin(2*np.pi*t) * np.exp(-t/10.)
-
+        yield t, 100 * np.sin(2*np.pi*t) * np.exp(-t/10.), t, 120 * np.sin(2.5*np.pi*t) * np.exp(-t/10.)
 
 def init():
     ax.set_ylim(40, 140)# change range!
     ax.set_xlim(0, 10)
     del xdata[:]
     del ydata[:]
+    del xdata2[:]
+    del ydata2[:]
     line.set_data(xdata, ydata)
-    return line,
+    line.set_data(xdata2, ydata2)
+    return line, line2,
 
 fig, ax = plt.subplots()
 line, = ax.plot([], [], lw=1)
+line2, = ax.plot([], [], lw=1)
 
 ax.set_title("Pulse and SpO2 Tracker")
 #ax.set_autoscaley_on(False)
@@ -34,21 +37,25 @@ ax.text(0.05, 0.85, text_spo2, transform=ax.transAxes, fontsize=14,
 
 #ax.grid() # add grid
 xdata, ydata = [], []
+xdata2, ydata2 = [], []
 
 
 def run(data):
     # update the data
-    t, y = data
+    t, y, t, y2 = data
     xdata.append(t)
     ydata.append(y)
+    xdata2.append(t)
+    ydata2.append(y2)
     xmin, xmax = ax.get_xlim()
 
     if t >= xmax:
         ax.set_xlim(xmin, 2*xmax)
         ax.figure.canvas.draw()
     line.set_data(xdata, ydata)
+    line2.set_data(xdata2, ydata2)
 
-    return line,
+    return line, line2,
 
 ani = animation.FuncAnimation(fig, run, data_gen, blit=False, interval=10,
                               repeat=False, init_func=init)
